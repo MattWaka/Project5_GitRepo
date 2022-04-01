c0808648d780336b46b3ab9912bbbdb61939215f
nahye.park <nahye.park@digipen.edu>
2022-03-31 17:09:16 -0700

    Merge branch 'main' of https://github.com/MattWaka/Project5_GitRepo into main

--- d3_templates/Proj7/js/main.js
+++ d3_templates/Proj7/js/main.js
@@ -2,9 +2,9 @@
     Header Here
  */
 
-var margin = { left:80, right:80, top:50, bottom:100 },
+    var margin = { left:80, right:100, top:50, bottom:100 },
     height = 500 - margin.top - margin.bottom, 
-    width = 900 - margin.left - margin.right;
+    width = 800 - margin.left - margin.right;
 
 var svg = d3.select("#chart-area")
     .append("svg")
@@ -16,243 +16,185 @@ var g = svg.append("g")
 
 var t = function(){ return d3.transition().duration(1000); }
 
+var parseTime = d3.timeParse("%d/%m/%Y");
+var formatTime = d3.timeFormat("%d/%m/%Y");
+var bisectDate = d3.bisector(function(d) { return d.date; }).left;
+
 // Add the line for the first time
-// g.append("path")
-//     .attr("class", "line")
-//     .attr("fill", "none")
-//     .attr("stroke", "grey")
-//     .attr("stroke-width", "3px");
+g.append("path")
+    .attr("class", "line")
+    .attr("fill", "none")
+    .attr("stroke", "grey")
+    .attr("stroke-width", "3px");
+
+// Labels
+var xLabel = g.append("text")
+    .attr("class", "x axisLabel")
+    .attr("y", height + 50)
+    .attr("x", width / 2)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("Time");
+var yLabel = g.append("text")
+    .attr("class", "y axisLabel")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -60)
+    .attr("x", -170)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("Price (USD)")
+
+// Scales
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
 
 // X-axis
-// var xAxisCall = d3.axisBottom()
-//     .ticks(4);
-// var xAxis = g.append("g")
-//     .attr("class", "x axis")
-//     .attr("transform", "translate(0," + height +")");
-
-// // Y-axis
-// var yAxisCall = d3.axisLeft()
-// var yAxis = g.append("g")
-//     .attr("class", "y axis");
-
-var capital_amount = [];
-
- // add the x Axis
- var x = d3.scaleLinear()
- .domain([-100, 500])
- .range([0, width]);
+var xAxisCall = d3.axisBottom()
+    .ticks(4);
 var xAxis = g.append("g")
-.attr("transform", "translate(0," + height + ")")
-.call(d3.axisBottom(x));
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height +")");
 
-var xAxisCall = d3.axisBottom();
-var yAxisCall = d3.axisLeft();
-
-// add the y Axis
-var y = d3.scaleLinear()
- .range([height, 0])
- .domain([0, 0.01]);
+// Y-axis
+var yAxisCall = d3.axisLeft()
 var yAxis = g.append("g")
-.call(d3.axisLeft(y));
+    .attr("class", "y axis");
 
-var numTrials = $("#numTrialsFromGUI").val(); 
-var numSim = $("#numSimsFromGUI").val(); 
-var percentBet = $("#percentToBetFromGUI").val();
-var winMulti = $("#winMultiplierFromGUI").val();
-var BlowupChance = $("#chanceOfBlowupFromGUI").val();
-var percentBlowup = $("#percentOfBetToBlowupFromGUI").val();
-var Binsize = $("#binNumberFromGUI").val();
-var kerenelEp = $("#kernelEpFromGUI").val();
-
-// get the initial data
-updateSimulation();
-
-var jitter_height = y.range();
-var jitter_width = 10;
-
-var dots = g.selectAll("dot")
-.data(capital_amount)
-.enter()
-.append("circle")
-.attr("cx", function(d){return(x(d) -5 + Math.random()*jitter_width )})
-.attr("cy", function(d){return(Math.random()*jitter_height[0])})
-.attr("r", 0.5)
-.style("fill", "black")
-.attr("stroke", "black")
-.style("opacity", 0.2);
-
-
-  // Features of the histogram
-  var histogram = d3.histogram()
-        .domain(y.domain())
-        .thresholds(y.ticks(20))    // Important: how many bins approx are going to be made? It is the 'resolution' of the violin plot
-        .value(d => d);
-
-// Compute kernel density estimation
-var kde = kernelDensityEstimator(kernelEpanechnikov(kerenelEp), x.ticks(Binsize))
-var density =  kde(capital_amount)
-
-// Plot the area
-var curve = g
-.append('g')
-.append("path")
-.attr("class", "mypath")
-.datum(density)
-.attr("fill", "#69b3a2")
-.attr("opacity", ".8")
-.attr("stroke", "#000")
-.attr("stroke-width", 1)
-.attr("stroke-linejoin", "round")
-.attr("d",  d3.line()
-.curve(d3.curveBasis)
-.x(function(d) { return x(d[0]); })
-.y(function(d) { return y(d[1]); })
-);
-
-// update the chart
-updateChart();
-    
 // Set event callbacks and listeners
-$("#numTrialsFromGUI").on("change",  function(d){
-    numTrials = this.value;
-    console.log("Num of Trials: " + numTrials);
-    updateSimulation();
-    updateChart();
-});
-$("#numSimsFromGUI").on("change",  function(d){
-    numSim = this.value;
-    console.log("Num of Sim: " + numSim);
-    updateSimulation();
-    updateChart();
-});
-$("#percentToBetFromGUI").on("change",  function(d){
-    percentBet = this.value;
-    console.log("Bet %: " + percentBet);
-    updateSimulation();
-    updateChart();
-});
-$("#winMultiplierFromGUI").on("change",  function(d){
-    winMulti = this.value;
-    console.log("Win Multiplier: " + winMulti);
-    updateSimulation();
-    updateChart();
-});
-$("#chanceOfBlowupFromGUI").on("change",  function(d){
-    BlowupChance = this.value;
-    console.log("Chance of Blowup" + BlowupChance);
-    updateSimulation();
-    updateChart();
-});   
-$("#percentOfBetToBlowupFromGUI").on("change",  function(d){
-    percentBlowup = this.value;
-    console.log("Percent of Blowup: " + percentBlowup);
-    updateSimulation();
-    updateChart();
-});  
-$("#binNumberFromGUI").on("change",  function(d){
-    Binsize = this.value;
-    console.log("Num of Bin: " + Binsize);
-    updateChart();
-});  
-$("#kernelEpFromGUI").on("change",  function(d){
-    kerenelEp = this.value;
-    console.log("Kernel Ep: " + kerenelEp);
-    updateChart();
-});  
+$("#coin-select").on("change", update)
+$("#var-select").on("change", update)
 
-function updateChart()
-{
-    kde = kernelDensityEstimator(kernelEpanechnikov(kerenelEp), x.ticks(Binsize))
-    density =  kde(capital_amount)
-    console.log(Binsize)
-    console.log(density)
+// Add jQuery UI slider
+$("#date-slider").slider({
+    range: true,
+    max: parseTime("31/10/2017").getTime(),
+    min: parseTime("12/5/2013").getTime(),
+    step: 86400000, // One day
+    values: [parseTime("12/5/2013").getTime(), parseTime("31/10/2017").getTime()],
+    slide: function(event, ui){
+        $("#dateLabel1").text(formatTime(new Date(ui.values[0])));
+        $("#dateLabel2").text(formatTime(new Date(ui.values[1])));
+        update();
+    }
+});
 
-    var maxX = Math.max(...capital_amount);
-    var maxY = Math.max(...density.map(function(x) {
-        return x[1];
-    }));
+d3.json("data/coins.json").then(function(data){
+    // console.log(data);
 
-    // update x-axis
-    x.domain([-100, maxX + 500]);
+    // Prepare and clean data
+    filteredData = {};
+    for (var coin in data) { //each coin type
+        if (!data.hasOwnProperty(coin)) { //if there is no data for that
+            continue;
+        }
+        filteredData[coin] = data[coin].filter(function(d){ //add new array with all present prices to new data
+            return !(d["price_usd"] == null)
+        });
+        filteredData[coin].forEach(function(d){ //convert
+            d["price_usd"] = +d["price_usd"];
+            d["24h_vol"] = +d["24h_vol"];
+            d["market_cap"] = +d["market_cap"];
+            d["date"] = parseTime(d["date"])
+        });
+    }
+
+    console.log(filteredData);
+
+    // we do not run d3.interval because we dont have any time-based automated tasks.
+
+    // Run the visualization for the first time
+    update();
+})
+
+function update() {
+
+    // Filter data based on selections
+    var coin = $("#coin-select").val(),
+        yValue = $("#var-select").val(),
+        sliderValues = $("#date-slider").slider("values");
+    var dataTimeFiltered = filteredData[coin].filter(function(d){
+        return ((d.date >= sliderValues[0]) && (d.date <= sliderValues[1]))
+    });
+
+    // Update scales
+    x.domain(d3.extent(dataTimeFiltered, function(d){ return d.date; }));
+    y.domain([d3.min(dataTimeFiltered, function(d){ return d[yValue]; }) / 1.005, 
+        d3.max(dataTimeFiltered, function(d){ return d[yValue]; }) * 1.005]);
+
+    // Fix for format values
+    var formatSi = d3.format(".2s");
+    function formatAbbreviation(x) {
+      var s = formatSi(x);
+      switch (s[s.length - 1]) {
+        case "G": return s.slice(0, -1) + "B";
+        case "k": return s.slice(0, -1) + "K";
+      }
+      return s;
+    }
+
+    // Update axes
     xAxisCall.scale(x);
     xAxis.transition(t()).call(xAxisCall);
-
-    // update y-axis
-    y.domain([0, maxY]);
     yAxisCall.scale(y);
-    yAxis.transition(t()).call(yAxisCall);
+    yAxis.transition(t()).call(yAxisCall.tickFormat(formatAbbreviation));
 
-    // update the chart
-    curve
-      .datum(density)
-      .transition()
-      .duration(1000)
-      .attr("d",  d3.line()
-        .curve(d3.curveBasis)
-          .x(function(d) { return x(d[0]); })
-          .y(function(d) { return y(d[1]); })
-      );
+    // Clear old tooltips
+    d3.select(".focus").remove();
+    d3.select(".overlay").remove();
 
-      Jitter();
-}
-
-
-function updateSimulation() {
-
-    //Calculate
-    capital_amount = [];
-    for(i = 0 ; i < numSim; ++i)
-    {
-        var money = 100;
-        for(j = 0; j < numTrials; ++j)
-        {
-            var bet = money * percentBet / 100;
-            money = money - bet;
-            var rand_num = Math.random() * 100.0;
-            if(rand_num < BlowupChance) //blowup
-            {
-                bet = bet - bet * percentBlowup/100.0;
-            }
-            else //win
-            {
-                bet = bet * winMulti;
-            }
-            money = money + bet;
-        }
-        console.log("Capital Money : " + money);
-        capital_amount.push(Math.floor(money));
+    // Tooltip code
+    var focus = g.append("g")
+        .attr("class", "focus")
+        .style("display", "none");
+    focus.append("line")
+        .attr("class", "x-hover-line hover-line")
+        .attr("y1", 0)
+        .attr("y2", height);
+    focus.append("line")
+        .attr("class", "y-hover-line hover-line")
+        .attr("x1", 0)
+        .attr("x2", width);
+    focus.append("circle")
+        .attr("r", 5);
+    focus.append("text")
+        .attr("x", 15)
+        .attr("dy", ".31em");
+    svg.append("rect")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr("class", "overlay")
+        .attr("width", width)
+        .attr("height", height)
+        .on("mouseover", function() { focus.style("display", null); })
+        .on("mouseout", function() { focus.style("display", "none"); })
+        .on("mousemove", mousemove);
+        
+    function mousemove() {
+        var x0 = x.invert(d3.mouse(this)[0]),
+            i = bisectDate(dataTimeFiltered, x0, 1),
+            d0 = dataTimeFiltered[i - 1],
+            d1 = dataTimeFiltered[i],
+            d = (d1 && d0) ? (x0 - d0.date > d1.date - x0 ? d1 : d0) : 0;
+        focus.attr("transform", "translate(" + x(d.date) + "," + y(d[yValue]) + ")");
+        focus.select("text").text(function() { return d3.format("$,")(d[yValue].toFixed(2)); });
+        focus.select(".x-hover-line").attr("y2", height - y(d[yValue]));
+        focus.select(".y-hover-line").attr("x2", -x(d.date));
     }
+
+    // Path generator
+    line = d3.line()
+        .x(function(d){ return x(d.date); })
+        .y(function(d){ return y(d[yValue]); });
+
+    // Update our line path
+    g.select(".line")
+        .transition(t)
+        .attr("d", line(dataTimeFiltered));
+
+    // Update y-axis label
+    var newText = (yValue == "price_usd") ? "Price (USD)" :
+        ((yValue == "market_cap") ?  "Market Capitalization (USD)" : "24 Hour Trading Volume (USD)")
+    yLabel.text(newText);
 }
 
-// Function to compute density
-function kernelDensityEstimator(kernel, X) {
-    return function(V) {
-      return X.map(function(x) {
-        return [x, d3.mean(V, function(v) { 
-            var temp2 = v;
-            return kernel(x - temp2); })];
-      });
-    };
-  }
-  function kernelEpanechnikov(k) {
-    return function(v) {
-        var temp = Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
-      return temp;
-    };
-}
 
-function Jitter()
-{
-    dots.remove();
 
-    dots = g.selectAll("dot")
-    .data(capital_amount)
-    .enter()
-    .append("circle")
-    .attr("cx", function(d){return(x(d) -5 + Math.random()*jitter_width )})
-    .attr("cy", function(d){return(Math.random()*jitter_height[0])})
-    .attr("r", 0.5)
-    .style("fill", "black")
-    .attr("stroke", "black")
-    .style("opacity", 0.2);
-}
